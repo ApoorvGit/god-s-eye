82ed325bc8b40c93c890cf7a28029523360faf7c
Apoorv Mishra <66416000+ApoorvGit@users.noreply.github.com>
2022-03-26 09:51:01 +0530

    Update and rename main.py to reco.py

--- Module-4/reco.py
+++ Module-4/reco.py
@@ -7,12 +7,17 @@ from simple_facerec import SimpleFacerec
 import sys
 sys.path.append('../Module-1')
 from voice import *
+sys.path.append('../')
+
 
 sfr = SimpleFacerec()
-sfr.load_encoding_images("images/")
+sfr.load_encoding_images("Module-4/images/")
+#sys.path.append('../')
+#print("abcd")
 def recognise(frame):
-    #frame = cv2.imread(frame, cv2.IMREAD_COLOR)
+    frame = cv2.imread(frame, cv2.IMREAD_COLOR)
     face_locations, names = sfr.detect_known_faces(frame)
     for name in names:
         #print(name)
         voice(name)
+#recognise('images/Apoorv.jpg')
